93b16b0131fd217ccacf109562db4882cc6d47cb
Ilia <ilia.stepanov07@gmail.com>
2025-06-21 17:22:39 +0400

    Update 0011 0150. Evaluate Reverse Polish Notation.py

--- 0011 0150. Evaluate Reverse Polish Notation.py
+++ 0011 0150. Evaluate Reverse Polish Notation.py
@@ -1,4 +1,4 @@
-class Solution:
+class SolutionUsingOneStack:
     def evalRPN(self, tokens: List[str]) -> int:
         def compute(left_value, right_value, token):
             if token == '+':
@@ -21,3 +21,35 @@ class Solution:
                 result = compute(left_value, right_value, token)
                 stack.append(result)
         return stack[0]
+
+
+class SolutionUsingRecursion:
+    def compute(self, left_value, right_value, token):
+        if token == '+':
+            return left_value + right_value
+        if token == '-':
+            return left_value - right_value
+        if token == '*':
+            return left_value * right_value
+        if token == '/':
+            return int(left_value / right_value)
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        operators = {'+', '-', '*', '/'}
+        def dfs():
+            token = tokens.pop()
+
+            if token not in operators:
+                return int(token)
+            
+            right_value = dfs()
+            left_value = dfs()
+
+            return self.compute(left_value, right_value, token)
+
+        return dfs()
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        return SolutionUsingRecursion().evalRPN(tokens)
