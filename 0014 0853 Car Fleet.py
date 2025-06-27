cad56991e34b48bf02c0beb2be376ed84bb7fb91
Ilia <ilia.stepanov07@gmail.com>
2025-06-27 21:29:39 +0400

    Create 0014 0853 Car Fleet.py

---
+++ 0014 0853 Car Fleet.py
@@ -0,0 +1,18 @@
+from math import ceil
+
+class Solution:
+  # not finished
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        ls = [(pos, speed[idx], ceil((target - pos) / speed[idx])) for idx, pos in enumerate(position)]
+        ls.sort()
+        stack = []
+        result = len(speed)
+
+        for idx, next_data in enumerate(ls):
+            next_pos, next_speed, next_time = next_data
+            while stack and stack[-1][2] <= next_time:
+                result -= 1
+                stack.pop()
+            stack.append(next_time)
+        return result
+
