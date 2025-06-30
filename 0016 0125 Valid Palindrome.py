7ce6bf2a314d2c4f6b74038a4c1fc76112a45706
Ilia <ilia.stepanov07@gmail.com>
2025-06-30 10:49:06 +0400

    Update 0016 0125 Valid Palindrome.py

--- 0016 0125 Valid Palindrome.py
+++ 0016 0125 Valid Palindrome.py
@@ -1,31 +1,59 @@
-class SolutionUsingReverseString:
-    def isPalindrome(self, s: str) -> bool:
-        corrected_s = []
-        for char in s:
-            if char.isalnum():
-                corrected_s.append(char.lower())
-        
-        return corrected_s == corrected_s[::-1]
+class SolutionUsingBruteForce:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        i = 0
+        while i < n:
+            j = i + 1
+            while j < n:
+                if numbers[i] + numbers[j] == target:
+                    return [i+1, j+1]
+                j += 1
+            i += 1
+        return [1, 2]
 
 
-class SolutionUsingSlidingWindow:
-    def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
-        s = s.lower()
-        while left < right:
-            if not s[left].isalnum():
-                left += 1
-            elif not s[right].isalnum():
-                right -= 1
-            elif s[left] != s[right]:
-                return False
+class SolutionUsingHashmap:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        hashmap = dict()
+        for idx, num in enumerate(numbers):
+            if num in hashmap:
+                hashmap[num].append(idx)
             else:
-                left += 1
-                right -= 1
-        return True
+                hashmap[num] = [idx]
+        for num1, idx1 in hashmap.items():
+            num2 = target - num1
+            if num2 not in hashmap:
+                continue
 
+            if num1 == num2:
+                idx2 = hashmap[num2][1]
+            else:
+                idx2 = hashmap[num2][0]
+
+            idx1 = idx1[0]
+            if idx2 > idx1:
+                return [idx1+1, idx2+1]
+            return [idx2+1, idx1+1]
+        return [1, 2]
+                
+        
+class SolutionUsingTwoPointes:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            curr_sum = numbers[left] + numbers[right]
+            if curr_sum > target:
+                right -= 1
+            elif curr_sum < target:
+                left += 1
+            else:
+                return [left+1, right+1]
+        return [1, 2]
+        
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        return SolutionUsingReverseString().isPalindrome(s)
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        return SolutionUsingTwoPointes().twoSum(numbers, target)
+        
