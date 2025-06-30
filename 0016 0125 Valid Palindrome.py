c26dbdc8f3916584eb0860f51ff1191015b88392
Ilia <ilia.stepanov07@gmail.com>
2025-06-30 10:13:42 +0400

    Update 0016 0125 Valid Palindrome.py

--- 0016 0125 Valid Palindrome.py
+++ 0016 0125 Valid Palindrome.py
@@ -1,4 +1,14 @@
-class Solution:
+class SolutionUsingReverseString:
+    def isPalindrome(self, s: str) -> bool:
+        corrected_s = []
+        for char in s:
+            if char.isalnum():
+                corrected_s.append(char.lower())
+        
+        return corrected_s == corrected_s[::-1]
+
+
+class SolutionUsingSlidingWindow:
     def isPalindrome(self, s: str) -> bool:
         left = 0
         right = len(s) - 1
@@ -13,5 +23,9 @@ class Solution:
             else:
                 left += 1
                 right -= 1
-            
         return True
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        return SolutionUsingReverseString().isPalindrome(s)
